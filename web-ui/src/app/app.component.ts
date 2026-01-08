eb5abb86a3c5b7d70e1a0ad55ef53dd1af12a886
Rory Gianni <hello@rgianni.cc>
2026-01-08 17:36:42 +0000

    upgrade angular

--- web-ui/src/app/app.component.ts
+++ web-ui/src/app/app.component.ts
@@ -3,13 +3,12 @@ import { MatIconRegistry } from "@angular/material/icon";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    imports: [
+        RouterOutlet,
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   constructor(private matIconRegistry: MatIconRegistry) {
