007475ff905f8589166c77c7d5b752c5ef59e122
Wei Duan <weiduan@google.com>
2026-03-04 13:42:14 -0800

    Internal change
    GitOrigin-RevId: 4da7461c24642b087206c9904e9efe1ad3c73443

--- src/models/custom_types.py
+++ src/models/custom_types.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -108,3 +108,9 @@ class OpinionResponseSchema(BaseModel):
 
   name: str
   subtopics: list[FlatTopic]
+
+
+class ScoreResponse(BaseModel):
+  """Schema for score generation for moderation and bridging."""
+
+  score: float = Field(description="The estimated probability (0.0 to 1.0).")
