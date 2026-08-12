039745ae6b7aa0245b6e8732b3244027eac05eca
Peter Wiegand <wiegandp@google.com>
2026-08-12 17:04:51 -0400

    Internal change
    GitOrigin-RevId: fa90e4fd90b8b91c05ba38458bc25327b435e9d9

--- src/report_ui/dev.js
+++ src/report_ui/dev.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mustache from "mustache";
 import fs from "fs";
+import { execSync } from "child_process";
 import browserSync from "browser-sync";
 
 const bs = browserSync.create();
@@ -10,8 +11,16 @@ const PORT = 3000;
 
 app.use(express.static("src"));
 
+function ensureData() {
+  if (!fs.existsSync("./temp/data-static.json")) {
+    console.log("temp/data-static.json not found, running build.js dev...");
+    execSync("node build.js dev", { stdio: "inherit" });
+  }
+}
+
 app.get("/", (req, res) => {
   try {
+    ensureData();
     const template = fs.readFileSync("./src/index.mustache", "utf8");
     const data = JSON.parse(fs.readFileSync("./temp/data-static.json", "utf8"));
 
@@ -28,7 +37,21 @@ app.listen(PORT, () => {
 
   bs.init({
     proxy: `http://localhost:${PORT}`,
-    files: ["src/**/*", "temp/**/*"],
+    files: [
+      "src/**/*",
+      "temp/**/*",
+      {
+        match: ["input/**/*"],
+        fn: function (event, file) {
+          console.log(`Input changed: ${file}. Re-running data processing...`);
+          try {
+            execSync("node build.js dev", { stdio: "inherit" });
+          } catch (err) {
+            console.error("Error rebuilding data:", err);
+          }
+        },
+      },
+    ],
     port: 3000,
     open: true,
     notify: false,
